491e21ec742fa1943ca0d0ac83f899dfa1a9fd33
godzbin <qq675402455@163.com>
2018-04-10 15:59:00 +0800

    调整写法

--- utils/dashBoard.js
+++ utils/dashBoard.js
@@ -20,9 +20,21 @@ const dashBoard = ({
         fontColor: '#B0D8F7', // 字体颜色
         textAlign: 'center'
     };
-    const arc = Math.PI / 180
-
     let p = Object.assign({}, dfp, position)
+    const {
+        x,
+        y,
+        radius,
+        articleWidth,
+        max,
+        color,
+        fontColor,
+        textAlign
+    } = p
+    const arc = Math.PI / 180,
+        PI2 = Math.PI * 2,
+        arc45 = arc * 45,
+        radiusToArticle = articleWidth / 2
     let animationRunIndex
     //  动画渲染
     const animation = (newValue) => {
@@ -51,7 +63,7 @@ const dashBoard = ({
         // 底部文字
         drawBottomTexts(ctx)
         if (!canvasId) {
-            console.log("canvasId 不能为空")
+            console.log('canvasId 不能为空')
             return
         }
         wx.drawCanvas({
@@ -70,138 +82,143 @@ const dashBoard = ({
         });
         return level
     }
-    // 关于值的圆弧
-    const drawRingToValue = (ctx, value, level) => {
-        let aw = p.articleWidth,
-            m = p.radius - aw / 2,
-            ring = 270 / p.max,
-            vr = value <= p.max ? (value * ring) : (p.max * ring),
-            startRrc = arc * 3 * 45,
-            endRrc = startRrc + (arc * vr);
+    // 当前值的圆弧
+    const drawRingToValue = (ctx, value, {
+        color
+    }) => {
+        const m = radius - radiusToArticle,
+            ring = 270 / max,
+            valueRing = (value < max ? value : max) * ring,
+            arcValueRing = arc * valueRing,
+            startRrc = arc45 * 3,
+            endRrc = startRrc + arcValueRing
         // 外圆弧
-        ctx.beginPath(0);
-        ctx.arc(p.x, p.y, m, startRrc, endRrc, false);
-        ctx.setLineWidth(aw);
-        ctx.setStrokeStyle(level.color);
-        ctx.stroke();
-        ctx.closePath();
-        ctx.setFillStyle(level.color);
-        ctx.setStrokeStyle(p.color);
+        ctx.beginPath(0)
+        ctx.arc(x, y, m, startRrc, endRrc, false)
+        ctx.setLineWidth(articleWidth)
+        ctx.setStrokeStyle(color)
+        ctx.stroke()
+        ctx.closePath()
+        ctx.setFillStyle(color)
+        ctx.setStrokeStyle(p.color)
         // 两个圆角
-        ctx.beginPath(0);
+        ctx.beginPath(0)
         // 结束圆角
-        ctx.arc(p.x - Math.cos(arc * (vr - 45)) * m,
-            p.y - Math.sin(arc * (vr - 45)) * m,
-            aw / 2,
-            endRrc,
-            endRrc + Math.PI * 2,
-            false);
-        ctx.fill();
-        ctx.closePath();
-        // 开始圆角
-        ctx.beginPath(0);
         ctx.arc(
-            p.x - Math.sin(startRrc) * m,
-            p.y - Math.cos(startRrc) * m,
-            aw / 2,
-            arc * 7 * 45,
-            arc * 15 * 45,
-            false);
-        ctx.fill();
-        ctx.closePath();
+            x - Math.cos(arcValueRing - arc45) * m,
+            y - Math.sin(arcValueRing - arc45) * m,
+            radiusToArticle,
+            endRrc,
+            endRrc + PI2,
+            false)
+        ctx.fill()
+        ctx.closePath()
+        // 开始圆角
+        ctx.beginPath(0)
+        ctx.arc(
+            x - Math.sin(startRrc) * m,
+            y - Math.cos(startRrc) * m,
+            radiusToArticle,
+            arc45 * 7,
+            arc45 * 15,
+            false)
+        ctx.fill()
+        ctx.closePath()
     };
     // 圆弧
     const drawRing = (ctx) => {
-        const arc45 = arc * 45,
-            articleWidth = p.articleWidth || dfp.articleWidth,
-            radiusToArticle = articleWidth / 2,
-            m = p.radius - articleWidth / 2,
+        const m = radius - radiusToArticle,
             startArc = arc45 * 3,
-            endArc = arc45;
+            endArc = arc45
         ctx.beginPath(0);
         // 弧
-        ctx.arc(p.x, p.y, p.radius - radiusToArticle, startArc, endArc + Math.PI * 2);
-        ctx.setLineWidth(articleWidth);
-        ctx.setStrokeStyle(p.color || dfp.color);
-        ctx.stroke();
-        ctx.closePath();
+        ctx.arc(x, y, m, startArc, endArc + PI2)
+        ctx.setLineWidth(articleWidth)
+        ctx.setStrokeStyle(color)
+        ctx.stroke()
+        ctx.closePath()
         // 两个圆角
-        ctx.setFillStyle(p.color || dfp.color);
-        ctx.setStrokeStyle(p.color || dfp.color);
-        ctx.beginPath(0);
+        ctx.setFillStyle(color)
+        ctx.setStrokeStyle(color)
+        ctx.beginPath(0)
         // 结束圆角
-        ctx.arc(p.x + Math.sin(endArc) * m, p.y + Math.cos(endArc) * m, radiusToArticle, endArc, endArc + arc45 * 5);
-        ctx.fill();
-        ctx.closePath();
+        ctx.arc(x + Math.sin(endArc) * m, y + Math.cos(endArc) * m, radiusToArticle, endArc, arc45 * 6)
+        ctx.fill()
+        ctx.closePath()
 
-        ctx.beginPath(0);
+        ctx.beginPath(0)
         // 开始圆角
-        ctx.arc(p.x - Math.sin(startArc) * m, p.y - Math.cos(startArc) * m, radiusToArticle, arc45 * 7, arc45 * 11);
-        ctx.fill();
-        ctx.closePath();
+        ctx.arc(x - Math.sin(startArc) * m, y - Math.cos(startArc) * m, radiusToArticle, arc45 * 7, arc45 * 11)
+        ctx.fill()
+        ctx.closePath()
     };
     // 中心数值
     const drawCenterValue = (ctx, value) => {
-        const fontSize = p.radius / 2.2,
-            text = value >= 0 ? value.toString() : "-",
+        const fontSize = radius / 2.2,
+            text = value >= 0 ? value.toString() : '-',
             textLength = text.length,
-            d = textLength > 2 ? textLength * fontSize / 3.2 : textLength * fontSize / 2.6,
-            level = getLevel(value);
-        ctx.setFontSize(fontSize);
-        ctx.setFillStyle(p.fontColor || dfp.fontColor);
-        ctx.setTextAlign(p.textAlign);
-        ctx.fillText(text, p.x - d / 2.2, p.y + fontSize / 4);
+            d = textLength * fontSize / (textLength > 2 ? 3.2 : 2.6),
+            level = getLevel(value)
+        ctx.setFontSize(fontSize)
+        ctx.setFillStyle(fontColor)
+        ctx.setTextAlign(textAlign)
+        ctx.fillText(text, x - d / 2.2, y + fontSize / 4)
         if (value >= 0) {
-            drawCorner(ctx, d + p.radius / 5, level);
+            drawCorner(ctx, d + radius / 5, level)
         }
-        drawUnit(ctx, d + p.radius / 5, unit);
+        drawUnit(ctx, d + radius / 5);
     }
     //  角标
-    const drawCorner = (ctx, d, level) => {
-        const name = level.name || "",
-            fs = p.radius / 6,
-            arc45 = arc * 45;
-        d = d / 1.5;
-        ctx.beginPath(0);
-        ctx.setFillStyle(level.color);
-        ctx.arc(p.x + d - fs / 1.5, p.y - fs, fs / 2, arc45 * 2, arc45 * 6);
-        ctx.arc(p.x + d + fs / 1.5, p.y - fs, fs / 2, arc45 * 6, arc45 * 10);
+    const drawCorner = (ctx, d, {
+        fontColor,
+        name = '',
+        color
+    }) => {
+        d = d / 1.5
+        const fs = radius / 6
+        ctx.beginPath(0)
+        ctx.setFillStyle(color)
+        ctx.arc(x + d - fs / 1.5, y - fs, fs / 2, arc45 * 2, arc45 * 6)
+        ctx.arc(x + d + fs / 1.5, y - fs, fs / 2, arc45 * 6, arc45 * 10)
         ctx.fill();
-        ctx.setFontSize(fs / 2.2);
-        ctx.setFillStyle(level.fontColor || dfp.fontColor);
-        ctx.setTextAlign(p.textAlign);
-        ctx.fillText(name, p.x + d, p.y - fs / 1.2);
+        ctx.setFontSize(fs / 2.2)
+        ctx.setFillStyle(fontColor)
+        ctx.setTextAlign(textAlign)
+        ctx.fillText(name, x + d, y - fs / 1.2)
     };
     // 单位
-    const drawUnit = (ctx, d, unit) => {
-        const fs = p.radius / 5;
-        ctx.setFontSize(fs / 2);
-        ctx.setFillStyle(p.fontColor);
-        ctx.setTextAlign(p.textAlign);
-        ctx.fillText(unit, p.x + d / 1.5, p.y + fs / 2.3);
+    const drawUnit = (ctx, d) => {
+        const fs = radius / 5
+        ctx.setFontSize(fs / 2)
+        ctx.setFillStyle(fontColor)
+        ctx.setTextAlign(textAlign)
+        ctx.fillText(unit, x + d / 1.5, y + fs / 2.3)
     };
     // 底部文字
     const drawBottomTexts = (ctx) => {
-        ctx.setFontSize(p.radius * 0.12);
-        ctx.setTextAlign(p.textAlign);
-
-        let y = p.y + p.radius * 0.72;
-        bottomTexts.map((item, i) => {
-            const text = (item.getTextToValue || "").replace(/{{value}}/g, item.value);
-            y += p.radius * 0.25 * i;
-            ctx.setFillStyle(item.color);
-            ctx.fillText(text, p.x, y);
+        ctx.setFontSize(radius * 0.12)
+        ctx.setTextAlign(textAlign)
+        let ty = y + radius * 0.72
+        bottomTexts.map(({
+            getTextToValue = '',
+            value,
+            color
+        }, i) => {
+            const text = (getTextToValue).replace(/{{value}}/g, value)
+            ty += radius * 0.25 * i
+            ctx.setFillStyle(color)
+            ctx.fillText(text, x, ty)
         });
     };
     const changeValue = (newValue) => {
-        animationRunIndex && clearTimeout(animationRunIndex);
-        animation(newValue);
+        animationRunIndex && clearTimeout(animationRunIndex)
+        animation(newValue)
     };
     const changePosition = (newPosition) => {
-        p = Object.assign(p, newPosition);
+        p = Object.assign(p, newPosition)
     }
     const setBottomTexts = (newBottomTexts) => {
-        bottomTexts = newBottomTexts;
+        bottomTexts = newBottomTexts
     }
     return {
         changeValue,
